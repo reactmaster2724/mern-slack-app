cd026f80cff2d05d4f0fe3a5f040132b87676fc0
reactmaster2724 <35813281+reactmaster2724@users.noreply.github.com>
2018-03-18 16:18:19 +0800

    change port

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: 'http: //localhost',
-  port: process.env.PORT || 3030,
+  port: process.env.PORT || 5000,
   originUrl: 'http://localhost:3000',
   mongoUrl: 'mongodb://localhost:27017/chat-app',
   ISDEV: process.env.NODE_ENV !== 'production'
